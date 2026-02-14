d482090422477665820c80b0ec990fe449c0b9b0
Jack Nash <jacknash42@gmail.com>
2026-02-15 00:41:08 +1000

    stuff

--- bridgeipelago.py
+++ bridgeipelago.py
@@ -667,7 +667,7 @@ async def CheckArchHost():
                 print("Port Check Failed")
                 print(RoomData["last_port"])
                 print(CoreConfig["ArchipelagoConfig"]['ArchipelagoPort'])
-                message = "Port Check Failed.  New port is " + str(RoomData["last_port"]) + "."
+                message = "Port Check Failed.  New port is " + str(RoomData["last_port"]) + "." + int(cond) + " - " + CoreConfig["ArchipelagoConfig"]['ArchipelagoPort']
                 #await MainChannel.send(message)
                 await DebugChannel.send(message)
                 SetConfigVariable("ArchipelagoPort", int(RoomData["last_port"]))
